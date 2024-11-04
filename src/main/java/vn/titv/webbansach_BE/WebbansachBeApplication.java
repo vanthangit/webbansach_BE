0f5a86689d18b30b24a61862820b4fd553f2fb62
Văn Thắng <nvthangforwork@gmail.com>
2024-11-04 17:26:25 +0700

    create project

---
+++ src/main/java/vn/titv/webbansach_BE/WebbansachBeApplication.java
@@ -0,0 +1,13 @@
+package vn.titv.webbansach_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbansachBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbansachBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/titv/webbansach_BE/WebbansachBeApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.titv.webbansach_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
